89f6afa7d0732511a52a2b0f4d0b9ce37156c607
Tep-Oli <126325772+Tep-Oli@users.noreply.github.com>
2024-05-17 11:42:13 +0200

    full branch cov

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -43,7 +43,7 @@ public class GT4500Test {
   public void fireTorpedo_All_Success(){
     // Arrange
 
-    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(false).thenReturn(true);
+    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(true);
     when(mockPTS.fire(1)).thenReturn(true);
     when(mockSTS.isEmpty()).thenReturn(false).thenReturn(true);
     when(mockSTS.fire(1)).thenReturn(true);
@@ -53,7 +53,7 @@ public class GT4500Test {
     // Assert
     assertEquals(true, result);
     verify(mockPTS,times(1)).fire(1);
-    verify(mockPTS,times(3)).isEmpty();
+    verify(mockPTS,times(4)).isEmpty();
 
     verify(mockSTS,times(1)).fire(1);
     verify(mockSTS,times(2)).isEmpty();
@@ -184,7 +184,7 @@ public class GT4500Test {
   public void fireTorpedo_All_Success_SEmpty(){
     // Arrange
 
-    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(true);
+    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(true);
     when(mockPTS.fire(1)).thenReturn(true);
     when(mockSTS.isEmpty()).thenReturn(true);
     // Act
@@ -193,7 +193,7 @@ public class GT4500Test {
     // Assert
     assertEquals(true, result);
     verify(mockPTS,times(2)).fire(1);
-    verify(mockPTS,times(4)).isEmpty();
+    verify(mockPTS,times(5)).isEmpty();
 
     verify(mockSTS,times(0)).fire(1);
     verify(mockSTS,times(2)).isEmpty();
@@ -217,5 +217,119 @@ public class GT4500Test {
 
     verify(mockSTS,times(2)).fire(1);
     verify(mockSTS,times(5)).isEmpty();
+ 
+  }
+
+  @Test
+  public void firedefault(){
+    // Arrange
+    // Act0
+    boolean result = ship.fireTorpedo(FiringMode.UNKNOWN);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Twice_SuccessFirst_FailSecond(){
+    // Arrange
+    when(mockPTS.isEmpty()).thenReturn(false);
+    when(mockPTS.fire(1)).thenReturn(true);
+    when(mockSTS.isEmpty()).thenReturn(false);
+    when(mockSTS.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(mockPTS,times(1)).fire(1);
+    verify(mockPTS,times(1)).isEmpty();
+
+    verify(mockSTS,times(0)).fire(1);
+    verify(mockSTS,times(0)).isEmpty();
+  }
+
+  @Test
+  public void fireTorpedo_Single_Fails(){
+    // Arrange
+    when(mockPTS.isEmpty()).thenReturn(true);
+    when(mockSTS.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+    verify(mockPTS,times(1)).isEmpty();
+
+    verify(mockSTS,times(1)).isEmpty();
+  }
+
+  @Test
+  public void fireTorpedo_Single_SuccesFirst_ThenFails(){
+    // Arrange
+    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(true);
+    when(mockPTS.fire(1)).thenReturn(true);
+    when(mockSTS.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(mockPTS,times(1)).isEmpty();
+    verify(mockPTS,times(1)).fire(1);
+
+    verify(mockSTS,times(0)).isEmpty();
+
+    // Act
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result2);
+    verify(mockPTS,times(2)).isEmpty();
+    verify(mockPTS,times(1)).fire(1);
+
+    verify(mockSTS,times(1)).isEmpty();
+  }
+
+  @Test
+  public void fireTorpedo_All_Fails(){
+    // Arrange
+
+    when(mockSTS.isEmpty()).thenReturn(false).thenReturn(true);
+    when(mockPTS.isEmpty()).thenReturn(true);
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+    verify(mockPTS,times(0)).fire(1);
+    verify(mockPTS,times(3)).isEmpty();
+
+    verify(mockSTS,times(0)).fire(1);
+    verify(mockSTS,times(3)).isEmpty();
+ 
+  }
+
+  @Test
+  public void fireTorpedo_All_Succes_branch(){
+    // Arrange
+
+    when(mockPTS.isEmpty()).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(true);
+    when(mockPTS.fire(1)).thenReturn(true);
+    when(mockSTS.isEmpty()).thenReturn(true);
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(true, result);
+    verify(mockPTS,times(1)).fire(1);
+    verify(mockPTS,times(5)).isEmpty();
+
+    verify(mockSTS,times(0)).fire(1);
+    verify(mockSTS,times(2)).isEmpty();
+ 
   }
 }
